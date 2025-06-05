e2c7361378eab964cfbcf8f65c8b3ea34e69a0da
Mohamed Imran RMN <141555789+mohamedimranrmn@users.noreply.github.com>
2025-06-06 03:28:14 +0530

    new EmployeeRoute.js

--- server/Routes/EmployeeRoute.js
+++ server/Routes/EmployeeRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from "jsonwebtoken";
-const bcrypt = require('bcryptjs');
+import bcrypt from "bcryptjs";
 import { Employee, EmployeeProject } from "../models/index.js";
 
 const router = express.Router();
