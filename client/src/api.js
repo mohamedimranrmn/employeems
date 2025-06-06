30698a92d2d43caef05b879c75b0e38731a0bbd5
Mohamed Imran <mohamed2003imran@gmail.com>
2025-06-06 17:32:10 +0530

    base url correction

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    withCredentials: true, // if you're using cookies/sessions
+    withCredentials: true,
 });
 
 export default api;
